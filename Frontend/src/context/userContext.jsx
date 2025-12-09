ea41eca849ac540811af094487e34aacbf236b60
Yash Pant <150167152+YashPant8055@users.noreply.github.com>
2025-12-09 22:38:02 +0530

    Update userContext.jsx

--- Frontend/src/context/userContext.jsx
+++ Frontend/src/context/userContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useEffect, useState } from "react";
 export const userDataContext = createContext(null);
 
 function UserContext({ children }) {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://virtual-ai-assistant-backend-8qjt.onrender.com";
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
   const [backendImage, setBackendImage] = useState(null);
